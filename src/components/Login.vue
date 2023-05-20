b4033d0c9281dce709b8b050f5a646ed51760329
Reign Buckingham <24620376+NemesisGuy@users.noreply.github.com>
2023-05-20 10:16:23 +0200

    added to front end, login register, nav ect..

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div class="form-container">
+        <form>
+        <h1 class="form-header">Login</h1>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" placeholder="Enter email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" placeholder="Enter password" id="password">
+            </div>
+            <button type="submit">Login</button>
+            <button @click="goToSignup">Signup</button>
+        </form>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "Login" // this is the name of the component
+    , methods: { // these are the methods that are called when the user clicks on the login button
+        goToSignup() {// this is a method that is called when the user clicks on the signup button
+            this.$router.push('/signup'); // this is a method that is called when the user clicks on the signup button
+        }
+    }, mounted() {
+        console.log('Login mounted'); // this is a lifecycle hook that is called when the component is mounted
+    },
+    data() {
+        return {
+            //get the data from the form
+            email: ""
+            ,
+            password: "" // this is the data that is passed to the backend for authentication it comes from the form
+        }
+    }
+
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="form-container">
+        <form>
+            <h2 class="form-header">Signup</h2>
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required>
+            </div>
+            <div class="form-group">
+                <label for="confirm-password">Confirm Password:</label>
+                <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password" required>
+            </div>
+            <button type="submit">Signup</button>
+
+            <button @click="goToLogin">Login</button>
+
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            username: '',
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        register() {
+            // Implement your registration logic here
+            // You can make an HTTP request to your backend API to handle the registration process
+            // You can use libraries like Axios or fetch to perform the HTTP request
+            // Example:
+            axios.post('http://localhost:8080/api/user/register', {
+               username: this.username,
+               email: this.email,
+               password: this.password
+             })
+             .then(response => {
+               // Handle success
+             })
+             .catch(error => {
+               // Handle error
+             });
+        }, goToLogin() {
+            this.$router.push('/login');
+        }
+    }
+};
+</script>
+
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
