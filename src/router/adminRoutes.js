1871eb45e913b8c008d0538d58ccb22f567550a0
Shamiso <shamiso@databalk.nu>
2023-09-18 10:55:08 +0200

    update

--- src/router/adminRoutes.js
+++ src/router/adminRoutes.js
@@ -1,14 +1,11 @@
 // Purpose: Contains all routes for the admin dashboard
 import AdminDash from "@/components/Admin/AdminDash.vue";
 import TestPage from "@/components/Main/TestPage.vue";
-<<<<<<< HEAD
+
 import ReturnRental from "@/components/Main/Rental/ReturnRental.vue";
 import CreateRental from "@/components/Admin/Rental/CreateRental.vue";
-// import Feedback from '/@components/Main/Feedback.vue';
-=======
-import Settings from "@/components/Admin/Settings/Settings.vue";
 
->>>>>>> d803c0a8732c0314ff2fefeb1b8ac186a69eda37
+import Settings from "@/components/Admin/Settings/Settings.vue";
 
 const adminRoutes = [
     {
