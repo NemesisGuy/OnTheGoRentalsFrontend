d3bd2b17fbda2ab6dd060de74530225956a3d374
Peter Buckingham <24620376+NemesisGuy@users.noreply.github.com>
2024-09-19 17:54:08 +0200

    fixed dependencies

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BootstrapVueNext } from 'bootstrap-vue-next';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
 
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './assets/Styles/styles.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
